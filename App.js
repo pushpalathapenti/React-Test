542f3c135ca5ca653d858bbf332e26a8535d15bd
pushpalathapenti <lpushpa808@gmail.com>
2025-02-22 06:24:15 +0530

    Creatin my own react app

--- App.js
+++ App.js
@@ -10,16 +10,20 @@
  *  </div>
  * </div>
  */
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
+    React.createElement("h1", {}, "I'm Raj"),
+    React.createElement("h2", {}, "I'm a Full Stack Developper"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "NAmaste"),
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
 ]);
+
+//due to this code is untidy, unmaintanable
 console.log(parent);
 // const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
 // console.log(heading);
